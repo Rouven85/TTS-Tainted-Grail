11519f3d288f13f80a9e9f62ab9b96b5858b5ae9
Rouven85 <r.arheidt@gmail.com>
2024-11-12 16:22:33 +0100

    241112_AutomizedEnergieCube004

--- ControlPanel.lua
+++ ControlPanel.lua
@@ -1,31 +1,26 @@
 -- AddAndSub Value from energie, Health or furcht --
 function energieCounter(player, value, id)
+    local playerBoard = getObjectFromGUID("ad0ab4")
+    local energieMarker = getObjectFromGUID("04e3d0")
+    local markerPosition = energieMarker.getPosition()
+    
+    local snapPoints = playerBoard.getSnapPoints()
+    local point1 = playerBoard.positionToWorld(snapPoints[28].position)
+    local point2 = playerBoard.positionToWorld(snapPoints[29].position)
+    
+    offset = point2[3] - point1[3] -- Distance between SnapPoints
     
     if value == "-1" then
         x = UI.getValue("energieValue") + 1
         UI.setValue("energieValue", x)
 
-        local playerBoard = getObjectFromGUID("ad0ab4")
-        local energieMarker = getObjectFromGUID("04e3d0")
-        local markerPosition = energieMarker.getPosition()
-        local markPos = playerBoard.positionToLocal(markerPosition)
-        
-        local boardPosition = playerBoard.getPosition()
-        local snapPoints = playerBoard.getSnapPoints()
-        local point1 = playerBoard.positionToWorld(snapPoints[28].position)
-        local point2 = playerBoard.positionToWorld(snapPoints[29].position)
-        
-        
-        offset = point2[3] - point1[3]
         energieMarker.setPosition(markerPosition + vector(0,0,offset))
-        log(offset)
     
-        
-       -- energieMarker.setPosition(offset)
-
     elseif value == "-2" then
         x = UI.getValue("energieValue") - 1
         UI.setValue("energieValue", x)
+
+        energieMarker.setPosition(markerPosition - vector(0,0,offset))
     end 
 end
 
