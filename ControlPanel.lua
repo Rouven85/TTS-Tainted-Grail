01678a8bdb1a8ca127de5ed21a93d5a44a751943
Rouven85 <r.arheidt@gmail.com>
2024-11-12 15:56:12 +0100

    241112_AutomizedEnergieCube003

--- ControlPanel.lua
+++ ControlPanel.lua
@@ -1,5 +1,5 @@
 -- AddAndSub Value from energie, Health or furcht --
-function energieCounter()
+function energieCounter(player, value, id)
     
     if value == "-1" then
         x = UI.getValue("energieValue") + 1
@@ -7,6 +7,21 @@ function energieCounter()
 
         local playerBoard = getObjectFromGUID("ad0ab4")
         local energieMarker = getObjectFromGUID("04e3d0")
+        local markerPosition = energieMarker.getPosition()
+        local markPos = playerBoard.positionToLocal(markerPosition)
+        
+        local boardPosition = playerBoard.getPosition()
+        local snapPoints = playerBoard.getSnapPoints()
+        local point1 = playerBoard.positionToWorld(snapPoints[28].position)
+        local point2 = playerBoard.positionToWorld(snapPoints[29].position)
+        
+        
+        offset = point2[3] - point1[3]
+        energieMarker.setPosition(markerPosition + vector(0,0,offset))
+        log(offset)
+    
+        
+       -- energieMarker.setPosition(offset)
 
     elseif value == "-2" then
         x = UI.getValue("energieValue") - 1
