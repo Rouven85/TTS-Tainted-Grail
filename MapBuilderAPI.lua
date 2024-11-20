3cdaa7898f5e0f2e42f85c95ec145e4726205a26
Rouven85 <r.arheidt@gmail.com>
2024-11-20 08:54:09 +0100

    241119_Creating Buttons onLoad 005

--- MapBuilderAPI.lua
+++ MapBuilderAPI.lua
@@ -4,77 +4,52 @@ function rightMapTile ()
 
 end
 
-function initSpawnedMap(object)
-    local obj = object
+function createButtonsForMapCard(obj, mapCardData)
+    local name = obj.getName()
+    local buttonsXML = ""
+    
+    if mapCardData[name] then
+        local cardParams = mapCardData[name]
+        local xr = -75
+        local xl = 75
+        local yd = 125
+        local yu = -125
 
-    if obj.hasTag("MapCard") then
-        local name = obj.getName()
-        
-        initLoadedMap()
-
-        if name == mapCardData[name].name then
-            local cardParams = mapCardData[name]
-            cardParams.guid = obj.getGUID()
-            log(cardParams.description)
-           
+        for _, id in ipairs(cardParams.mapID) do
+            if id.right then
+                buttonsXML = buttonsXML .. string.format([[<Button width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xr)
+            end
+            if id.down then
+                buttonsXML = buttonsXML .. string.format([[<Button width="150" height="50" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yd)
+            end
+            if id.up then
+                buttonsXML = buttonsXML .. string.format([[<Button width="200" height="100" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yu)
+            end
+            if id.left then
+                buttonsXML = buttonsXML .. string.format([[<Button width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xl)
+            end
         end
+        
+        -- Panel erstellen und Knöpfe hinzufügen
+        local initXML = [[<Panel></Panel>]]
+        local combinedXML = initXML:gsub("</Panel>", buttonsXML .. "</Panel>")
+        obj.UI.setXml(combinedXML)
     end
 end
 
 function initLoadedMap()
-    local obj = getAllObjects()
-    for _,v in ipairs(obj) do
-        
-        if v.hasTag("MapCard") then
-            local name = v.getName()
-            local bounds = v.getBounds()
-            log(bounds)
-            if mapCardData[name] ~= nil then
-                local xValue = ((bounds.center[1] *(100)/2)+25) 
-                local xr = tostring(xValue) 
-                
-                local xValue = ((bounds.center[1] *(100)/2)) 
-                local xl = tostring(xValue) 
-                
-                local yValue = ((bounds.center[2] *(100))+20)
-                local yd = tostring(yValue+0.5)
-                
-                local yValue = ((bounds.center[2] *(100)))
-                local yu = tostring(yValue+0.5)
-                
-                local initXML = [[<Panel></Panel>]]
-                v.UI.setXml(initXML)
-                local buttonsXML = ""
-                for _, id in ipairs(mapCardData[name].mapID) do   
-                    if id.right ~= nil then
-                        buttonsXML = buttonsXML .. string.format([[<Button width = "50" height = "150" position = "%s 0 -100" rotation = "0 0 180">Hallo</Button>]],xr)
-                    else
-                        buttonsXML = buttonsXML ..[[]]             
-                    end
-                    
-                    if id.down ~= nil then
-                        buttonsXML = buttonsXML .. string.format([[<Button width = "150" height = "50" position = "0 %s -100" rotation = "0 0 180">Hallo</Button>]],yd)
-                    else
-                        buttonsXML = buttonsXML ..[[]] 
-                    end
-
-                    if id.up ~= nil then
-                        buttonsXML = buttonsXML .. string.format([[<Button width = "200" height = "100" position = " 0 %s -100" rotation = "0 0 180">Hallo</Button>]],yu)
-                    else
-                        buttonsXML = buttonsXML ..[[]]                
-                    end
-                    
-                    if id.left ~= nil then
-                        buttonsXML = buttonsXML .. string.format([[<Button width = "50" height = "150" position = "%s 0 -100" rotation = "0 0 180">Hallo</Button>]],xl)
-                    else
-                        buttonsXML = buttonsXML ..[[]]
-                    end   
-                end 
-                local combinedXML = initXML:gsub("</Panel>",buttonsXML .."</Panel>")
-                v.UI.setXml(combinedXML)    
-            end
-        end    
+    for _, obj in ipairs(getAllObjects()) do
+        if obj.hasTag("MapCard") then
+            createButtonsForMapCard(obj, mapCardData)
+        end
     end
- end
- 
-                    
+end
+
+function initSpawnedMap(object)
+    Wait.time(function()
+        if object.hasTag("MapCard") then
+            createButtonsForMapCard(object, mapCardData)
+        end
+    end, 0.1)
+end
+                 
