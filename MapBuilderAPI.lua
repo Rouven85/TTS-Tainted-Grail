b5d69b52e05b56217089784f87a3dbb970dccf35
Rouven85 <r.arheidt@gmail.com>
2024-11-20 14:10:30 +0100

    241119_Creating Buttons onLoad 007

--- MapBuilderAPI.lua
+++ MapBuilderAPI.lua
@@ -15,29 +15,29 @@ function createButtonsForMapCard(obj, mapCardData)
         local yd = 125
         local yu = -125
 
-        -- Baue die Buttons in einer Schleife
+        -- Create Buttons --
         for _, id in ipairs(cardParams.mapID) do
             if id.right then
-                buttonsXML = buttonsXML .. string.format([[<Button onClick="right_BtnClick" width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xr)
+                buttonsXML = buttonsXML .. string.format([[<Button onClick="Global/right_BtnClick" width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xr)
             end
             if id.down then
-                buttonsXML = buttonsXML .. string.format([[<Button onClick="down_BtnClick" width="150" height="50" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yd)
+                buttonsXML = buttonsXML .. string.format([[<Button onClick="Global/down_BtnClick" width="150" height="50" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yd)
             end
             if id.up then
-                buttonsXML = buttonsXML .. string.format([[<Button onClick="up_BtnClick" width="200" height="100" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yu)
+                buttonsXML = buttonsXML .. string.format([[<Button onClick="Global/up_BtnClick" width="200" height="100" position="0 %s -100" rotation="0 0 180">Hallo</Button>]], yu)
             end
             if id.left then
-                buttonsXML = buttonsXML .. string.format([[<Button onClick="left_BtnClick" width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xl)
+                buttonsXML = buttonsXML .. string.format([[<Button onClick="Global/left_BtnClick" width="50" height="150" position="%s 0 -100" rotation="0 0 180">Hallo</Button>]], xl)
             end
         end    
 
-        -- Kombiniere XML und setze es auf einmal
+        -- combine XML and set XML --
         local combinedXML = string.format([[<Panel>%s</Panel>]], buttonsXML)
         obj.UI.setXml(combinedXML)
 
         -- Logge nach der Verzögerung
         Wait.time(function()
-            log("XML nach dem Setzen (mit Verzögerung): " .. obj.UI.getXml())
+            
         end, 0.5)  -- Verzögerung von 0.5 Sekunden
     end
 end
