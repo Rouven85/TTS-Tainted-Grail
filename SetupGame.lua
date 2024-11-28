f4d265050ebbea85559e4f14cd810df8ed1f66b6
Rouven85 <r.arheidt@gmail.com>
2024-11-28 15:21:37 +0100

    241124_AutoSetup 1.0

--- SetupGame.lua
+++ SetupGame.lua
@@ -1,5 +1,3 @@
--- require("/Libraries/tblChapterCards")
-
 
 local w1 = "White Encounter 1"
 local w2 = "White Encounter 2"
@@ -20,9 +18,9 @@ local p4 = "Purple Encounter 4"
 
 local spawnedSetupCard = nil
 
+
 function createSetupBtn(object)
     spawnedSetupCard = object
-    log (object.type)
     if object.hasTag("SetupCard") then
         object.UI.setXmlTable({
             {
@@ -58,9 +56,6 @@ function sortAndFilterEncounterDecks (...)
     local encounterList = {}
     local filterValues = {...}
     
-
-
-    
     for _,obj in ipairs(allObjects) do
         if obj.hasTag("EncounterDeck") then 
             for _, lvl in ipairs(filterValues) do  
@@ -74,14 +69,7 @@ function sortAndFilterEncounterDecks (...)
     return (encounterList)  
 end
 
---[[ function kapitel1 ()
-    local encounterList = sortAndFilterEncounterDecks(w2,b1, g1, p1)
-    placeEncouterDecks(encounterList)
-    
-end --]]
-
 function placeEncouterDecks(encounterList)
-    --log (encounterList)
     local targetAreas ={
         ["White Encounter 1"] = "e9c965",
         ["Green Encounter 1"] = "c8c49d",
@@ -100,29 +88,37 @@ function placeEncouterDecks(encounterList)
         ["Blue Encounter 4"] = "135e69",
         ["Purple Encounter 4"] = "19b21c",
     }
-    local delay = 0
+    local areaGUID = {}
+    local loadedDecks = {}
     for k,objGUID in pairs(encounterList) do
-        
-        --log (obj)
         for name, areaGUID in pairs(targetAreas) do 
-            
             if k == name then
                 local deck = getObjectFromGUID(objGUID)
                 local area = getObjectFromGUID(areaGUID)
-
                 local currentDeck = deck
                 local currentPosition = area.getPosition()
                 local cloneDeck = currentDeck.clone({
                     position = currentPosition})
-                --deck.setPosition(area.getPosition())
-                --Wait.time (function() cloneDeck.setPosition(currentPosition) end, 0.3)
-                Wait.time(function() cloneDeck.shuffle() end, 0.1, 3)
-            end   
-        end
+                    cloneDeck.setName("Encounter Deck")
+            end     
+        end 
     end
+    shuffleDecks ()
 end
 
-
+function shuffleDecks ()
+    Wait.time(function() 
+        local allObjects = getAllObjects()
+    for _,encounter in ipairs(allObjects) do
+        local name = encounter.getName()
+        if name == "Encounter Deck" then
+            local obj = getObjectFromGUID(encounter.getGUID())
+            obj.shuffle()   
+        end
+    end
+    end, 0.5, 3)
+    allObjects = nil
+end
 
 
 function setupGame()
@@ -138,23 +134,63 @@ function setupGame()
         ["Kapitel 3"] = function ()
             local encounterList = sortAndFilterEncounterDecks(w1, w2, b1, b2, g1, g2, p1, p2)
             placeEncouterDecks(encounterList) 
-        end}
-
+        end,
+        ["Kapitel 4"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w1, w2, b1, b2, g1, g2, p1, p2)
+            placeEncouterDecks(encounterList)
+        end,
+        ["Kapitel 5"] = function () 
+            local encounterList = sortAndFilterEncounterDecks(w1, w2, w3, b1, b2, b3, g1, g2, g3, p1, p2, p3)
+            placeEncouterDecks(encounterList)
+        end, 
+        ["Kapitel 6"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w1, w2, w3, b1, b2, b3, g1, g2, g3, p1, p2, p3)
+            placeEncouterDecks(encounterList) 
+        end,
+        ["Kapitel 7"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w2, w3, b2, b3, g2, g3, p2, p3)
+            placeEncouterDecks(encounterList)
+        end,
+        ["Kapitel 8"] = function () 
+            local encounterList = sortAndFilterEncounterDecks(w2, w3, b2, b3, g2, g3, p2, p3)
+            placeEncouterDecks(encounterList)
+        end, 
+        ["Kapitel 9"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w2, w3, b2, b3, g2, g3, p2, p3)
+            placeEncouterDecks(encounterList) 
+        end,
+        ["Kapitel 10"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w2, w3, w4, b2, b3, b4, g2, g3, g4, p2, p3, p4)
+            placeEncouterDecks(encounterList)
+        end,
+        ["Kapitel 11"] = function () 
+            local encounterList = sortAndFilterEncounterDecks(w2, w3, w4, b2, b3, b4, g2, g3, g4, p2, p3, p4)
+            placeEncouterDecks(encounterList)
+        end, 
+        ["Kapitel 12"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w3, w4, b3, b4, g3, g4, p3, p4)
+            placeEncouterDecks(encounterList) 
+        end,
+        ["Kapitel 13"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w3, w4, b3, b4, g3, g4, p3, p4)
+            placeEncouterDecks(encounterList)
+        end,
+        ["Kapitel 14"] = function () 
+            local encounterList = sortAndFilterEncounterDecks(w3, w4, b3, b4, g3, g4, p3, p4) -- Stufe 5 Karten ergänzen
+            placeEncouterDecks(encounterList)
+        end, 
+        ["Kapitel 15"] = function ()
+            local encounterList = sortAndFilterEncounterDecks(w1, w2, b1, b2, g1, g2, p1, p2) -- Stufe 5 Karten ergänzen
+            placeEncouterDecks(encounterList) 
+        end,
+    }
     if spawnedSetupCard then
         local kapitelName = spawnedSetupCard.getName()
         for key,kapitel in pairs(kapitelList) do
-            
             if key == kapitelName then
                 kapitelList[kapitelName]()
-                log(key)
             end
-            
-            
-            
         end
-        
-       --[[  local encounterList = sortAndFilterEncounterDecks(w2,b1, g1, p1)
-        placeEncouterDecks(encounterList) --]]
    end
 end
     
