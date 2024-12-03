ec7c16f0bb65a250b3e116a8f61830a402db5a37
Rouven85 <r.arheidt@gmail.com>
2024-12-03 12:24:30 +0100

    241203_ClassesCharakters 002

--- CharakterControl.lua
+++ CharakterControl.lua
@@ -39,68 +39,78 @@ function Charakter.new(object)
     return self
 end
 
-function Charakter:setAttributes()
-    local markerBag = getObjectFromGUID("c3ba04")
+function Charakter:getAttributeSnaps()
     local snaps = self.object.getSnapPoints()
     local filteredSnaps = {}
-    local i = 0
-    for  _,snap in ipairs(snaps) do
-        
-        if snap.tags[1] == "AgrSnap" then
-            if self.aggression ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
-        end
-        if snap.tags[1] == "MutSnap" then
-            if self.mut ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
-        end
-        if snap.tags[1] == "PragSnap" then
-            if self.pragmatik ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
-        end
-        if snap.tags[1] == "MitSnap" then
-            if self.mitgefuehl ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
-        end
-        if snap.tags[1] == "VorSnap" then
-            if self.vorsicht ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
-        end
-        if snap.tags[1] == "SpiritSnap" then
-            if self.spirit ~= nil then
-                local marker = markerBag.takeObject({
-                    position = self.object.positionToWorld(snap.position),
-                    smooth = false,        -- Verhindert das Herunterfallen
-                    snap_to = true         -- Aktiviert das Snapping
-                })
-            end
+    local snapDictonary ={}
+    for _,snap in ipairs(snaps) do
+        if snap ~= nil and snap.tags[1] then
+                local tagName = snap.tags[1]
+                local position = snap.position
+                filteredSnaps[tagName] = position
         end
     end
+    return filteredSnaps
+end
+
+function Charakter:setAttributes(snaps)
+    local markerBag = getObjectFromGUID("c3ba04")
+    if self.spirit ~= nil then
+        local spiritMarkers = 0  -- Zähler für gesetzte Marker
+
+        for tagName, position in pairs(snaps) do
+            if tagName == "SpiritSnap" then
+                spiritMarkers = spiritMarkers + 1
+                
+                -- Holen eines Markers aus der Marker-Tasche
+                local marker = markerBag.takeObject({position = self.object.positionToWorld(position), smooth = true})
+
+                -- Überprüfen, ob die Anzahl der Marker erreicht ist
+                if spiritMarkers >= self.spirit then
+                    break
+                end
+            end
+        end
+   
+    end
+   
+   
+  --[[   if self.spirit ~= nil and self.spirit == 1 then
+        for _, snap in ipairs(snaps) do
+            log(snap)
+        end
+    end --]]
+end
+
+ --[[   local marker = markerBag.takeObject({
+                    position = self.object.positionToWorld(snap),
+                    smooth = false,        -- Verhindert das Herunterfallen
+                    snap_to = true         -- Aktiviert das Snapping
+                    })
+                log("Hallo") --]]
+
+    --log (filteredSnaps)
+       --[[  if self.spirit ~= nil and self.spirit ~= #filteredSnaps then
+            log(snap)
+        end  --]]
+        
+            --table.insert(filteredSnaps, snap)
+       
+        --[[ if snap.tags[1] == "SpiritSnap" then
+            table.insert(filteredSnaps, snap)
+            
+            --log(#filteredSnaps)
+            if #filteredSnaps < self.spirit and self.spirit ~= nil then
+                --log(self.spirit)
+                local marker = markerBag.takeObject({
+                    position = self.object.positionToWorld(snap.position),
+                    smooth = false,        -- Verhindert das Herunterfallen
+                    snap_to = true         -- Aktiviert das Snapping
+                })
+            end
+        end --]]
+    
+    --log(filterdSnaps)
     --local yOffset = filteredSnaps[2].position.z - filteredSnaps[1].position.z
     
     --markerBag.takeObject(self.position + vector(0, 2, 0))
@@ -125,7 +135,7 @@ function Charakter:setAttributes()
         end
     }) --]]
     --self.position 
-end
+
 -- Methode der Klasse
 function Charakter:print()
     log("Hallo, ich bin " .. self.name)
@@ -145,7 +155,9 @@ function getObjects()
             
             elseif object.getName() == "Sloan" then
                 sloan = Charakter.new(getObjectFromGUID(object.guid))
-                sloan:setAttributes()
+                local snaps = sloan:getAttributeSnaps()
+                sloan:setAttributes(snaps)
+                --log(attributes)
             elseif object.getName() == "Fyul" then
                 fyul = Charakter.new(getObjectFromGUID(object.guid)) 
             end
